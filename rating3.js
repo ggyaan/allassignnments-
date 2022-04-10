054de0b17b10256e01f450963844eb4905ca7aee
ggyaan <96737222+ggyaan@users.noreply.github.com>
2022-04-10 13:00:50 +0530

    Update rating3.js

--- rating3.js
+++ rating3.js
@@ -2,23 +2,23 @@
 //1. Find the element with rating >= 3 
 
 
-let user=superMarket.filter((element) => element.rating >= 3);
+let user=superMarket.find((element) => element.rating >= 3);
 console.log(user);
 
 //2. Find the element with type ‘vegetable’ 
 
-let users=superMarket.filter((element) => element.type == 'vegetable');
+let users=superMarket.find((element) => element.type == 'vegetable');
 console.log(users);
 
 //3. Find the element with price < 15 
 
-let user2=superMarket.filter((element) => element.price < 15);
+let user2=superMarket.find((element) => element.price < 15);
 console.log(user2);
 
 //4. Find the element with title which contains the word ‘Lemon’
 
-let users3=superMarket.filter((element) => element.type == 'lemon');
-console.log(users3);
+let withElement= superMarket.find((element) => element.title.includes('lemon'));
+console.log(withElement);
 
 //5. Find the element with height = 450 and width = 450 
 
